6f6df98a232b7718711dd924db0ed003566d960e
MominSahil <67797239+MominSahil@users.noreply.github.com>
2020-10-17 16:58:44 -0400

    dropdown looks ok

--- scripts/script.js
+++ scripts/script.js
@@ -9,6 +9,17 @@ $('a[href*=\\#]').on('click', function(e){
     }, 1000);
 });
 
+$('.menu__dropdown').on('click', function (e) {
+    e.preventDefault();
+    $('.menu').css("display", "flex");
+    $('header').toggleClass('menu__hideDropdown');
+    console.log("clicking fine");
+})
+
+// $('div:not(.menu__dropdown)').on('click', function() {
+//     $('header').removeClass('menu__hideDropdown');
+// })
+
 myApp.init = () => {
     console.log("Hire me and I will remove this console log.");
 }
