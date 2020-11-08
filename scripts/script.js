5c4c70dd569d7f68c78d95a900f0f88163cf49ba
MominSahil <67797239+MominSahil@users.noreply.github.com>
2020-11-08 16:04:58 -0500

    dropdown switch

--- scripts/script.js
+++ scripts/script.js
@@ -4,6 +4,15 @@ const myApp = {
 
 $('a[href*=\\#]').on('click', function(e){
     e.preventDefault();
+    // const animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
+    // const animationName = 'animated shake'
+    $('header').removeClass('menu__hideDropdown');
+    // $('main section').addClass('showHide');
+    // $(this.hash).removeClass('showHide');
+    // $(this.hash).addClass(animationName).one(animationEnd, function() {
+    //     $(this.hash).removeClass(animationName);
+    // })
+
     $('html, body').animate({
         scrollTop : $(this.hash).offset().top
     }, 1000);
@@ -15,7 +24,7 @@ $('.menu__dropdown i').on('click', function (e) {
     $('header').toggleClass('menu__hideDropdown');
 })
 
-$('div:not(.menu__dropdown').on('click', function() {
+$('section').on('click', function() {
     $('header').removeClass('menu__hideDropdown');
 })
 
