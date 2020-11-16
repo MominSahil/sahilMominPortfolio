d1f93bdb28dd2441010d622023c1ed481fc71eb0
MominSahil <67797239+MominSahil@users.noreply.github.com>
2020-11-15 20:15:45 -0500

    skills media query issue fixed

--- scripts/script.js
+++ scripts/script.js
@@ -1,37 +1,31 @@
-const myApp = {
+const myApp = {};
 
-}
+$("a[href*=\\#]").on("click", function (e) {
+  e.preventDefault();
+  $("header").removeClass("menu__hideDropdown");
 
-$('a[href*=\\#]').on('click', function(e){
-    e.preventDefault();
-    // const animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
-    // const animationName = 'animated shake'
-    $('header').removeClass('menu__hideDropdown');
-    // $('main section').addClass('showHide');
-    // $(this.hash).removeClass('showHide');
-    // $(this.hash).addClass(animationName).one(animationEnd, function() {
-    //     $(this.hash).removeClass(animationName);
-    // })
-
-    $('html, body').animate({
-        scrollTop : $(this.hash).offset().top
-    }, 1000);
+  $("html, body").animate(
+    {
+      scrollTop: $(this.hash).offset().top,
+    },
+    1000
+  );
 });
 
-$('.menu__dropdown i').on('click', function (e) {
-    e.preventDefault();
-    $('.menu').css("display", "flex");
-    $('header').toggleClass('menu__hideDropdown');
-})
+$(".menu__dropdown i").on("click", function (e) {
+  e.preventDefault();
+  $(".menu").css("display", "flex");
+  $("header").toggleClass("menu__hideDropdown");
+});
 
-$('section').on('click', function() {
-    $('header').removeClass('menu__hideDropdown');
-})
+$("section").on("click", function () {
+  $("header").removeClass("menu__hideDropdown");
+});
 
 myApp.init = () => {
-    console.log("HIRE ME!");
-}
+  console.log("HIRE ME!");
+};
 
-$(document).ready(function(){
-    myApp.init();
-})
+$(document).ready(function () {
+  myApp.init();
+});
