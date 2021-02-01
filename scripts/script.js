51fcc4d027523daa88866b413886e49b88ae6e10
MominSahil <67797239+MominSahil@users.noreply.github.com>
2021-01-31 21:31:39 -0500

    svg animation added

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,10 @@
 const myApp = {};
 
+const svgName = $(".home__name path");
+for (let i = 0; i < svgName.length; i++) {
+  console.log(`${i + 1} = ${svgName[i].getTotalLength()}`);
+}
+
 $("a[href*=\\#]").on("click", function (e) {
   e.preventDefault();
   $("header").removeClass("menu__hideDropdown");
@@ -16,14 +21,22 @@ $(".menu__dropdown i").on("click", function (e) {
   e.preventDefault();
   $(".menu").css("display", "flex");
   $("header").toggleClass("menu__hideDropdown");
+  $(".fa-times").toggleClass("menu__dropdown--close");
+  $(".fa-bars").toggleClass("menu__dropdown--open");
 });
 
 $("section").on("click", function () {
   $("header").removeClass("menu__hideDropdown");
+  $(".fa-times").removeClass("menu__dropdown--close");
 });
 
 myApp.init = () => {
-  console.log("HIRE ME!");
+  let typed = new Typed(".home__work--typed", {
+    strings: ["Front-End Developer", "Engineer", "Volunteer, Eager Beaver"],
+    typeSpeed: 50,
+    backSpeed: 50,
+    loop: true,
+  });
 };
 
 $(document).ready(function () {
